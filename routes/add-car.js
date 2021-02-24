5bcd0493c54e0b311796c4db99227dceb4303409
repl.it user <replituser@example.com>
2021-02-24 21:18:00 +0000

    Added post request to addcar route

--- routes/add-car.js
+++ routes/add-car.js
@@ -1,15 +1,16 @@
 const resData = [];
 const route=(data)=> {
     'use strict';
-    var addCarRoutes = require('express').Router();
+		const express = require("express");
+    var addCarRoutes = express.Router();
     addCarRoutes.route("/")
     .get((req, res,next)=> {
         // res.render("")
         res.json(data)        
     })
     .post((req,res)=>{
-
-        console.log(req.body);
+				console.log(req.body);
+        resData.push(req.body);
     })   
     return addCarRoutes;
 };
