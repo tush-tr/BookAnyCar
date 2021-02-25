3e3787ed4b87a73761a4f58b56a501ab055251c6
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-02-25 08:48:58 +0000

    documeted code for main script

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const ejs = require("ejs");
-const app = express();
-const bodyParser = require("body-parser");
-const { static } = require("express");
-app.set('view engine', 'ejs');
+// TODO: Import modules
+const express = require("express"); 
+const ejs = require("ejs"); // template engine
+const app = express(); 
+const bodyParser = require("body-parser"); // for parsing request bodies
+const { static } = require("express");     // static files handling
+
+// TODO: Importing Middlewares
+
+
+// TODO: Express specific stuff
+app.set('view engine', 'ejs');  
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(bodyParser.json());   
+app.use(express.static("public")); 
 
 // TODO: Importing Routes
 const homeRoutes = require("./routes/home");
@@ -14,6 +20,8 @@ const addCarRoutes = require("./routes/add-car");
 const carsListRoutes = require("./routes/cars");
 
 
+
+// Initializing array for data of cars
 const data = [{
         car: "Alto",
         model: "2018",
@@ -25,6 +33,10 @@ const data = [{
         fair: "$400"
     }
 ]
+
+
+
+
 // Home Routes
 app.use("/", homeRoutes);
 // addcar routes -- take data from user of car _POST
@@ -35,6 +47,7 @@ app.use("/cars",carsListRoutes(data));
 // TODO: API
 app.use("/api/addcar",require("./routes/apipost")(data));
 app.use("/api/cars",require("./routes/apiget")(data));
+
 
 
 app.listen(3500,()=>{
