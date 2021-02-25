1e8ae873af4daa4be74e6d63cdf19d899dc93493
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-02-25 10:34:11 +0000

    Implemented image upload feature in add car route

--- routes/add-car.js
+++ routes/add-car.js
@@ -1,14 +1,31 @@
-module.exports=(data)=> {
+const path = require('path');
+// TODO: Importing Middlewares
+const Resize = require('../middleware/resize');
+const upload = require('../middleware/uploadmiddleware');
+
+function CarData(car, model, fair, img) { this.car = car; this.model = model; this.fair = fair; this.img = img };
+
+module.exports = (data) => {
     'use strict';
-	const express = require("express");
+    const express = require("express");
     var addCarRoutes = express.Router();
     addCarRoutes.route("/")
-    .get((req,res)=>{
-        res.render("add-car")
-    })
-    .post((req,res)=>{
-		console.log(req.body);
-        data.push(req.body);
-    })   
+        .get((req, res) => {
+            res.render("add-car")
+        })
+        .post(upload.single('image'), async function (req, res) {
+            const imagePath = path.join(__dirname, '../public/car-images');
+            const fileUpload = new Resize(imagePath);
+            if (!req.file) {
+                res.status(401).json({ error: 'Please provide an image' });
+            }
+            const filename = await fileUpload.save(req.file.buffer);
+            let addData = new CarData(req.body.car,req.body.model, req.body.fair,filename);
+            data.push(addData);
+            res.redirect("/");
+        });
     return addCarRoutes;
 };
+
+// console.log(req.body);
+// data.push(req.body);
